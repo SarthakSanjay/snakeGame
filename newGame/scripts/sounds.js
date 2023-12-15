18523694cd1ea76a79f63f8a604d836baf133c25
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-15 23:11:22 +0530

    make file to export sounds

---
+++ newGame/scripts/sounds.js
@@ -0,0 +1,8 @@
+//sounds
+let eatSound = new Audio("/audio/eat.mp3")
+eatSound.volume = 1
+let gameOverSound = new Audio("/audio/gameOver.mp3")
+let bgm = new Audio("/audio/gameBAckground.mp3")
+let buttonClickSound = new Audio("/audio/buttonClick.mp3")
+//---------------------------------------------------------------------------
+export {eatSound , gameOverSound, bgm , buttonClickSound}
