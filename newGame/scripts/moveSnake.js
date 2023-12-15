601cde1ce84fdc932c7e8f70f63f6bdf80417d3d
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2023-12-15 23:09:46 +0530

    add move snake funcion

---
+++ newGame/scripts/moveSnake.js
@@ -0,0 +1,7 @@
+export function moveSnake(snakeArr , inputDir) {
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] };
+    }
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
+}
